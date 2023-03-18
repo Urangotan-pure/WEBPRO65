02deb2a2011d671df7ea75d874056e1f404fa0cc
Urangotan-pure <88420671+Urangotan-pure@users.noreply.github.com>
2023-03-18 11:31:53 +0700

    week5

---
+++ WEEK05-VUE-1/ex1/ex1.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el : '#ex1',
+    data :{
+        student_id : '64070183',
+        student_fname : 'punnaporn',
+        student_lname : 'jungpiumsuk',
+    }
+})
---
+++ WEEK05-VUE-1/ex1/ex2-7.js
@@ -0,0 +1,66 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        number_runner: 0, // ค่าเริ่มต้นจะเป็น 0
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        week: [
+            {
+                title: 'Sunday',
+                color: '#ff0000'
+            },
+            {
+                title: 'Monday',
+                color: '#ffcc00'
+            },
+            {
+                title: 'Tuesday',
+                color: '#ff66ff'
+            },
+            {
+                title: 'Wendesday',
+                color: '#009933'
+            },
+            {
+                title: 'Thursday',
+                color: '#ff6600'
+            },
+            {
+                title: 'Friday',
+                color: '#3399ff'
+            },
+            {
+                title: 'Saturday',
+                color: '#9900ff'
+            },
+        ],
+        counter: 0,
+        show_dropdown:false
+    },
+    methods: {
+        decrease() {
+            this.counter -= 1;
+        },
+        increase() {
+            this.counter += 1;
+        },
+        Dropdown_t_or_f() {
+            this.show_dropdown = !this.show_dropdown;
+        },
+        callColor(day){
+            return{
+                color : day.color
+            }
+        }
+    },
+    
+
+    // *** SPOIL เนื้อหาสัปดาห์ถัด ๆ ไป***
+
+    mounted() {
+        // วนไปเรื่อย ๆ ทุก ๆ 1 วินาที
+        setInterval(() => {
+            // แสดงค่าหลักสุดท้ายของวินาที
+            this.number_runner = new Date().getSeconds() % 6;
+        }, 1000);
+    },
+})
